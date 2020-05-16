79188751aa961ad393fdc2552db868e9293a76ac
Evan Kerik <ekerik220@gmail.com>
2020-05-16 14:58:08 +0900

    Added confirm email requirement on registration

--- models/User.js
+++ models/User.js
@@ -33,6 +33,10 @@ const userSchema = new mongoose.Schema({
     ],
     default: [],
   },
+  emailConfirmed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { registerValidation, loginValidation } = require("../validation");
 const verify = require("../middlewares/verifytoken");
+const nodemailer = require("nodemailer");
 
 // REGISTER A USER
 router.post("/register", async (req, res) => {
@@ -25,6 +26,35 @@ router.post("/register", async (req, res) => {
     email: req.body.email,
     password: hashedPassword,
   });
+
+  // Create confirmation link that we can email the user
+  const token = jwt.sign({ name: user.name }, process.env.TOKEN_SECRET);
+  const link = "http://localhost:4000/user/confirm/" + token;
+
+  // Send an email with the confirmation link
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL, // generated ethereal user
+      pass: process.env.EMAIL_PASS, // generated ethereal password
+    },
+  });
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: user.email,
+    subject: "Please confirm your email.",
+    text: `Click this link to confirm your email: ${link}`,
+  };
+  transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("Email sent!");
+  });
+
   try {
     const savedUser = await user.save();
     res.send({ user: user._id });
@@ -50,6 +80,31 @@ router.post("/login", async (req, res) => {
   // Create and assign a token
   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
   res.header("auth-token", token).send(token);
+});
+
+// CONFIRM USER EMAIL
+router.post("/confirm/:token", async (req, res) => {
+  const token = req.params.token;
+  const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+  const userName = verified.name;
+
+  const user = await User.findOne({ name: userName });
+  if (!user)
+    return res.status(400).send("No user matching this confirmation token.");
+
+  if (user.emailConfirmed)
+    return res
+      .status(400)
+      .send("This user's email has already been confirmed.");
+
+  user.emailConfirmed = true;
+
+  try {
+    const savedUser = await user.save();
+    res.send({ user: user._id });
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 module.exports = router;
