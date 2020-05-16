393f8043bed733db6053ef93e8a00abe00469d12
Evan Kerik <ekerik220@gmail.com>
2020-05-16 11:29:48 +0900

    Add delete item endpoint and get list endpoint

--- routes/list.js
+++ routes/list.js
@@ -3,6 +3,8 @@ const User = require("../models/User");
 const verify = require("./verifytoken");
 
 // ADD ITEM TO USER'S LIST
+// header: "auth-token" = user's jwt-token
+// body: JSON with item_name, category, variation (optional)
 router.post("/add", verify, async (req, res) => {
   const user = await User.findOne({ _id: req.user._id });
   if (!user) return res.status(400).send("No user matching this token.");
@@ -24,9 +26,59 @@ router.post("/add", verify, async (req, res) => {
       variations: req.body.variation ? [req.body.variation] : [],
     });
   }
-  user.save((err, doc) => {
-    if (err) return res.status(400).send(err);
-    else res.send(doc);
+
+  try {
+    const savedUser = await user.save();
+    res.json(savedUser);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// DELETE ITEM FROM USER'S LIST
+// header: "auth-token" = user's jwt-token
+// body: JSON with item_name, category, variation (optional)
+// If variation provided, deletes that variation. If not provided, deletes entire item.
+router.post("/delete", verify, async (req, res) => {
+  const user = await User.findOne({ _id: req.user._id });
+  if (!user) return res.status(400).send("No user matching this token.");
+
+  const matchingIndex = user.list.findIndex(
+    (ele) => ele.item_name === req.body.item_name
+  );
+
+  if (matchingIndex === -1)
+    return res.status(400).send("Cannot delete item that is not in the list.");
+
+  if (!req.body.variation) user.list.splice(matchingIndex, 1);
+  else {
+    const delIndex = user.list[matchingIndex].variations.indexOf(
+      req.body.variation
+    );
+    if (delIndex === -1)
+      return res
+        .status(400)
+        .send("Cannot delete variation that is not in the list");
+
+    user.list[matchingIndex].variations.splice(delIndex, 1);
+  }
+
+  try {
+    const savedUser = await user.save();
+    res.json(savedUser);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// GET the list of user with given user name.
+router.get("/:userName", async (req, res) => {
+  const user = await User.findOne({ name: req.params.userName });
+  if (!user) return res.status(400).send("No user matching this user name.");
+
+  res.json({
+    name: user.name,
+    list: user.list,
   });
 });
 
