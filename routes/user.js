e6f5dbc83ea8bb00da41ac7073cf77b9afc9cd90
Evan K <ekerik220@gmail.com>
2020-06-13 09:54:30 +0900

    Refactored to make sendEmail a separate function - Reason: Need to send emails from more than one place in code. Avoid duplicate code.

--- routes/user.js
+++ routes/user.js
@@ -142,12 +142,7 @@ router.post("/resend/:email", async (req, res) => {
   res.status(200).send({ message: "Resent confirmation email." });
 });
 
-const sendConfirmationEmail = (to) => {
-  // Create confirmation link that we can email the user
-  const token = jwt.sign({ email: to }, process.env.TOKEN_SECRET);
-  const link = "http://localhost:3000/confirm/" + token;
-
-  // Send an email with the confirmation link
+const sendEmail = (to, subject, message) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     host: "smtp.gmail.com",
@@ -161,8 +156,8 @@ const sendConfirmationEmail = (to) => {
   const mailOptions = {
     from: process.env.EMAIL,
     to: to,
-    subject: "Please confirm your email.",
-    text: `Click this link to confirm your email: ${link}`,
+    subject: subject,
+    text: message,
   };
   transporter.sendMail(mailOptions, (err, data) => {
     if (err) {
@@ -171,5 +166,57 @@ const sendConfirmationEmail = (to) => {
     console.log("Email sent!");
   });
 };
+
+const sendConfirmationEmail = (to) => {
+  // Create confirmation link that we can email the user
+  const token = jwt.sign({ email: to }, process.env.TOKEN_SECRET);
+  const link = "http://localhost:3000/confirm/" + token;
+
+  const subject = "Please confirm your email.";
+  const message = `Click this link to confirm your email: ${link}`;
+  sendEmail(to, subject, message);
+};
+
+// FORGOT PASSWORD SEND EMAIL
+router.post("/forgotpassword/:email", async (req, res) => {
+  const user = await User.findOne({ email: req.params.email });
+  if (!user)
+    return res.status(400).send({ error: "No user matching this email." });
+
+  const token = jwt.sign(
+    { email: req.params.email },
+    process.env.TOKEN_SECRET,
+    { expiresIn: "1h" }
+  );
+
+  const password_reset_link = "http://localhost:3000/passwordreset/" + token;
+
+  const subject = "ACNH Tracker - Password reset request";
+  const message = `Please click this link to begin resetting your password: ${password_reset_link}`;
+  sendEmail(req.params.email, subject, message);
+  res.status(200).send({ message: "Sent password reset email." });
+});
+
+// CHANGE PASSWORD
+router.post("/changepassword/:token", async (req, res) => {
+  const token = req.params.token;
+  jwt.verify(token, process.env.TOKEN_SECRET, async function (err, decoded) {
+    if (err) console.log(err);
+    if (decoded) {
+      const email = decoded.email;
+      const newPassword = req.body.newPassword;
+      const user = await User.findOne({ email: email });
+
+      // Hash the password
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+      user.password = hashedPassword;
+      const savedUser = await user.save();
+      console.log(savedUser);
+      res.send({ message: "Updated password." });
+    }
+  });
+});
 
 module.exports = router;
